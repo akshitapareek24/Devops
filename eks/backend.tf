19dd21cd986dbc0029be9875173a8d08e47431df
akshitapareek24 <33574439+akshitapareek24@users.noreply.github.com>
2023-06-07 10:41:41 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "akshita-humanitec-demo"
-    key            = "terraform-aws-eks-workshop.tfstate"
+    key            = "akshita-humanitec-demo.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
